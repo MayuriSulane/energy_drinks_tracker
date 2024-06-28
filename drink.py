a66c6e0717e7e96367ab5b52c52fd31ab51b70a7
ola <ola@example.com>
2024-06-28 16:18:40 +0530

    updated code

--- drink.py
+++ drink.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import pandas as pd
 
 # for holding the resultant list
 element_list = []
@@ -16,24 +17,33 @@ input_element = driver.find_element(By.ID, "twotabsearchtextbox")
 input_element.clear()
 input_element.send_keys("energy drink" + Keys.ENTER)
 
-for page in range(1, 2, 1):
-    
-    driver.get(f"https://www.amazon.in/s?k=energy+drink&page={page}")
-    
-    # Give time for the page to load
-    time.sleep(3)
+# Give time for the page to load
+time.sleep(3)
 
-    # Update these selectors to match the actual HTML structure
-    titles = driver.find_elements(By.CSS_SELECTOR, "span.a-text-normal")
-    prices = driver.find_elements(By.CSS_SELECTOR, "span.a-price-whole")
-    # descriptions = driver.find_elements(By.CSS_SELECTOR, "span.a-text-normal")  # Adjust as necessary
+
+# Update these selectors to match the actual HTML structure
+titles = driver.find_elements(By.CSS_SELECTOR, "span.a-size-base-plus")
+prices = driver.find_elements(By.CSS_SELECTOR, "span.a-price-whole")
+
+# ratings = driver.find_elements(By.CSS_SELECTOR, "span.a-icon-alt")
+
+length = min(len(titles), len(prices))
+
+for i in range(length):
+    title = titles[i].text.strip().replace(",","")
+    price = prices[i].text.strip().replace(",","")
+    # rating = ratings[i].get_attribute("innerHTML").strip().replace(",", "")  # Remove commas from rating
     
-    for i in range(len(titles)):
-        title = titles[i].text if i < len(titles) else 'N/A'
-        price = prices[i+1].text if i+1 < len(prices) else 'N/A'
-        # description = descriptions[i].text if i < len(descriptions) else 'N/A'
-        element_list.append([title, price])
+    element_list.append([title, price])
     
-    driver.close()
-    for item in element_list:
-        print(item)
+
+# for i,item in enumerate(titles):
+#     element_list=[item.text,prices[i].text]
+#     print(element_list)
+    
+driver.close()   
+    
+df = pd.DataFrame(element_list, columns=['Title' , 'Price'])
+df.to_csv("energy_drink.csv", index=False)
+
+
